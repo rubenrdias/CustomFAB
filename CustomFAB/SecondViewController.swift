5b200d2e4ab1a307c65b6f4aa109c7478df1ee86
Ruben Dias <ruben.roque.dias@gmail.com>
2020-03-24 09:38:46 +0000

    Added some images and text for visual appeal.

--- CustomFAB/SecondViewController.swift
+++ CustomFAB/SecondViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        setupFAB()
+        setupPhotoText()
+    }
+    
+    private func setupFAB() {
         let button = FAB(frame: CGRect(x: 0, y: 0, width: FAB.buttonHeight, height: FAB.buttonHeight))
         button.addToController(self)
     }
-
-    //    "Photo by Joe Hepburn on Unsplash"
-    //    "https://unsplash.com/@thatjoebloke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-    //    "https://unsplash.com/s/photos/interesting?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    
+    private func setupPhotoText() {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 15, weight: .regular),
+            .foregroundColor: UIColor.label
+        ]
+        
+        let attributedString = NSMutableAttributedString(string: "Photo by Nijwam Swargiary on Unsplash", attributes: attributes)
+        attributedString.addAttribute(.link, value: "https://unsplash.com/@pixel_talkies?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", range: NSRange(location: 9, length: 16))
+        attributedString.addAttribute(.link, value: "https://unsplash.com/s/photos/interesting?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", range: NSRange(location: 29, length: 8))
+        
+        photoTextView.attributedText = attributedString
+    }
 }
--- CustomFAB/ViewController.swift
+++ CustomFAB/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var photoTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupFAB()
+        setupPhotoText()
+    }
+    
+    private func setupFAB() {
         let button = FAB(frame: CGRect(x: 0, y: 0, width: FAB.buttonHeight, height: FAB.buttonHeight))
         button.addToController(self)
-        
     }
-
-//    "Photo by Nijwam Swargiary on Unsplash"
-//    "https://unsplash.com/@pixel_talkies?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-//    "https://unsplash.com/s/photos/interesting?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+    
+    private func setupPhotoText() {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 15, weight: .regular),
+            .foregroundColor: UIColor.label
+        ]
+        
+        let attributedString = NSMutableAttributedString(string: "Photo by Joe Hepburn on Unsplash", attributes: attributes)
+        attributedString.addAttribute(.link, value: "https://unsplash.com/@thatjoebloke?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", range: NSRange(location: 9, length: 11))
+        attributedString.addAttribute(.link, value: "https://unsplash.com/s/photos/interesting?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText", range: NSRange(location: 24, length: 8))
+        
+        photoTextView.attributedText = attributedString
+    }
 }
